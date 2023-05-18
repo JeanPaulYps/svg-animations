f62a4085af8f86f8c01f4d8bb365a77c56de1dd2
Jean Paul <yjeanpaul@gmail.com>
2023-05-18 11:03:31 -0500

    Added morph demo

---
+++ pages/morph-demo/script.ts
@@ -0,0 +1,6 @@
+import KUTE from "kute.js";
+
+const morph = KUTE.to("#Egg", { path: "#Pear" }, { duration: 1000 }).start();
+// const showTip = KUTE.to("#Tip", { strokeDashoffset: 0 });
+
+// morph.chain(showTip).start();
